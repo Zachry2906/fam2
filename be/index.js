bf1eedcaba3b6efe9281150a295a4284724cf847
Arya <aryazakaria67@gmail.com>
2025-05-26 13:47:21 +0700

    ck

--- be/index.js
+++ be/index.js
@@ -7,19 +7,20 @@ import "dotenv/config";
 import { initializeModels } from './model/index.js'; 
 
 const app = express();
-app.set("view engine", "ejs");
 
+// Trust the first proxy (Google Cloud Run's load balancer)
+// This is important for 'secure: true' cookies to work correctly
+app.set('trust proxy', 1); 
+app.set("view engine", "ejs");
 app.use(cookieParser());
 
 app.use(cors({
- origin: "https://fe-077-dot-b-07-452412.uc.r.appspot.com",
+ origin: ["https://fe-077-dot-b-07-452412.uc.r.appspot.com", "http://127.0.0.1:5500"],
  credentials: true,
  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
  allowedHeaders: ["Content-Type", "Authorization"],
  optionsSuccessStatus: 200
 })); 
-
-app.options('*', cors());
 
 app.use(express.json());
 
